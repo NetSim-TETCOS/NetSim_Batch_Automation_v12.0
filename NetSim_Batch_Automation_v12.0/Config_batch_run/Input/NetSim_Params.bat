ba25aecdc8f3f9248b0bf5cd20207b218784c553
NetSim-TETCOS <sales@tetcos.com>
2020-01-21 19:27:43 +0530

    Initial commit

---
+++ NetSim_Batch_Automation_v12.0/Config_batch_run/Input/NetSim_Params.bat
@@ -0,0 +1,9 @@
+@echo off
+SET DIR="%2"
+SET NETSIM_PATH="%1"
+
+CD %2
+
+%1\NetSimCore.exe -apppath %1 -iopath  %2 -license 5053@192.168.0.9
+
+exit
---
+++ README.md
@@ -0,0 +1,2 @@
+# NetSim_Batch_Automation_v12.0
+ Automate the process of running multiple simulations one after the other
